e5888c4970478b90d6e76a93537c8be0cecce771
mkrasnansk <miso.krasnansky@gmail.com>
2022-11-26 23:52:36 +0100

    sgds

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const port = process.env.PORT;
 
 console.log(port);
 
+// AWS.config.update({ region: "eu-central-1" });
+
+
 app.use(express.urlencoded());
 app.use(bodyParser.json());
 
@@ -18,12 +21,12 @@ app.use(bodyParser.json());
 //    // origin: ["https://www.owee.sk", "www.owee.sk", "owee.sk", "https://owee.sk", "https://owee-15664.firebaseapp.com", "https://owee-15664.web.app", "https://www.tiendapepe.sk"],
 // };
 // app.use(cors(corsOptions));
-// app.use((req, res, next) => {
-//    res.setHeader("Access-Control-Allow-Origin", "*");
-//    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-//    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//    next();
-// });
+app.use((req, res, next) => {
+   res.setHeader("Access-Control-Allow-Origin", "*");
+   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});
 
 const sesClient = new SESClient();
 

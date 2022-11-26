4dc35d7a34aeddf02afedcdb0f7861d28cc3908c
mkrasnansk <miso.krasnansky@gmail.com>
2022-11-26 23:45:00 +0100

    without cors

--- app.js
+++ app.js
@@ -13,21 +13,21 @@ console.log(port);
 app.use(express.urlencoded());
 app.use(bodyParser.json());
 
-const corsOptions = {
-   origin: "*",
-   // origin: ["https://www.owee.sk", "www.owee.sk", "owee.sk", "https://owee.sk", "https://owee-15664.firebaseapp.com", "https://owee-15664.web.app", "https://www.tiendapepe.sk"],
-};
-app.use(cors(corsOptions));
-app.use((req, res, next) => {
-   res.setHeader("Access-Control-Allow-Origin", "*");
-   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
-});
+// const corsOptions = {
+//    origin: "*",
+//    // origin: ["https://www.owee.sk", "www.owee.sk", "owee.sk", "https://owee.sk", "https://owee-15664.firebaseapp.com", "https://owee-15664.web.app", "https://www.tiendapepe.sk"],
+// };
+// app.use(cors(corsOptions));
+// app.use((req, res, next) => {
+//    res.setHeader("Access-Control-Allow-Origin", "*");
+//    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+//    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//    next();
+// });
 
 const sesClient = new SESClient();
 
-app.post("/mail", cors(corsOptions), (req, res) => {
+app.post("/mail",  (req, res) => {
    let data = req.body;
 
    const html_contents = `
